27b772214c2e545c7e48664aecd4d86abbf31013
Manish Verma <vermam318@gmail.com>
2021-07-06 16:20:35 +0530

    google fontfamily added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ai_radio/pages/home_page.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AI Radio',
-      theme: ThemeData(),
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
